688296eb7cb4d1bdbc88e58d85d73d42ea9dfc88
znguyen <znguyen@getraline.com>
2023-05-19 06:32:54 +0200

    Modify model and controller to add post into db

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -13,29 +13,46 @@ class AdminController extends AbstractController
 
         if (isset($_POST['submitAddButton']))
         {
-            if (!empty($_POST['title']) && !empty($_POST['excerpt']) && !empty($_POST['content']) && !empty($_POST['photo']) && !empty($_FILES['image']['name']))
+            if (!empty($_POST['title']) && !empty($_POST['excerpt']) && !empty($_POST['content']) && !empty($_POST['category']))
             {
+                if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0) {
+                    $file = $_FILES["photo"];
+
+                    // Specify the directory to which you want to save the uploaded image
+                    $targetDir = "assets/img/";
+
+                    // Check extension format
+                    $extensions = ['.png','.jpg','.jpeg','.gif','.PNG','.JPG','.JPEG','.GIF'];
+                    $extension = strrchr($file["name"], '.');
+                    if (!in_array($extension,$extensions))
+                        throw new \Exception ("Cette image n'est pas valable");
+
+                    // Generate a unique name for the image to avoid conflicts
+                    $fileName = uniqid() . "_" . $file["name"];
+
+                    // Create the full path of the target file
+                    $targetFilePath = $targetDir . $fileName;
+
+                    // Move the uploaded file to the target location
+                    if(!move_uploaded_file($file["tmp_name"], $targetFilePath))
+                        throw new \Exception('Impossible de télécharger la photo');
+                }
+
                 $aData = [
                     'title' => $_POST['title'],
                     'excerpt' => $_POST['excerpt'],
                     'content' => $_POST['content'],
-                    'author_id' => '',
-                    'category_id' => ''
+                    'author_id' => '1',
+                    'category_id' => $_POST['category'],
+                    'photo' => $fileName
                     // 'created_date' => date('Y-m-d H:i:s')
                 ];
+
                 $success = $adminModel->addPost($aData);
                 if (!$success)
                     throw new \Exception('Impossible d\'ajouter un article');
                 else
-                    header('Location: index.php');
-
-                $file = $_FILES['photo']['name'];
-                $extensions = ['.png','.jpg','.jpeg','.gif','.PNG','.JPG','.JPEG','.GIF'];
-                $extension = strrchr($file, '.');
-                if (!in_array($extension,$extensions))
-                    throw new \Exception ("Cette image n'est pas valable");
-
-                $adminModel->postImg($_FILES['photo']['tmp_name'], $extension);
+                    header('Location: blog');
             }
             else
                 throw new \Exception('Tous les champs doivent être remplis.');
--- src/Models/AdminModel.php
+++ src/Models/AdminModel.php
@@ -17,27 +17,16 @@ class AdminModel
     public function addPost($aData)
     {
         $sql = $this->Db->prepare(
-            "INSERT INTO Post (title, excerpt, content, author_id, category_id)
-            VALUES (:title, :excerpt, :content, :author_id, :category_id)"
+            "INSERT INTO Post (title, excerpt, content, author_id, category_id, photo)
+            VALUES (:title, :excerpt, :content, :author_id, :category_id, :photo)"
         );
         $sql->bindValue(':title', $aData['title'], \PDO::PARAM_STR);
         $sql->bindValue(':excerpt', $aData['excerpt'], \PDO::PARAM_STR);
         $sql->bindValue(':content', $aData['content'], \PDO::PARAM_LOB);
         $sql->bindValue(':author_id', $aData['author_id'], \PDO::PARAM_INT);
         $sql->bindValue(':category_id', $aData['category_id'], \PDO::PARAM_INT);
+        $sql->bindValue(':photo', $aData['photo'], \PDO::PARAM_STR);
         return $sql->execute();
-    }
-
-    public function postImg($tmp_name, $extension)
-    {
-      $i = [
-        'id'     => $this->Db->lastInsertId(),
-        'photo'  => $this->Db->lastInsertId().$extension
-      ];
-
-      $oStmt = $this->Db->prepare('UPDATE Post SET photo = :photo WHERE id = :id');
-      move_uploaded_file($tmp_name,"assets/img/".$i['photo']);
-      return $oStmt->execute($i);
     }
 
     public function getCategories()
