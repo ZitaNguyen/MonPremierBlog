8db28bf9952e1e0d97318938176e665a0bf8c73c
znguyen <znguyen@getraline.com>
2023-06-28 05:34:04 +0200

    Add function to send mail from contact form

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -9,6 +9,35 @@ class HomeController extends AbstractController
 {
     public function displayHomePage()
     {
+        if (isset($_POST['submitContactFormButton']))
+        {
+            if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message']))
+            {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $message = $_POST['message'];
+
+                // Set up the email
+                $to = 'zitanguyen84@gmail.com';
+                $subject = "New Contact Form Submission";
+                $body = "Name: " . $name . "\n";
+                $body .= "Email: " . $email . "\n";
+                $body .= "Message: " . $message;
+
+                // Set headers
+                $headers = "From: " . $email . "\r\n";
+                $headers .= "Reply-To: " . $email . "\r\n";
+
+                // Send the email
+                if (mail($to, $subject, $body, $headers)) {
+                    echo "Email sent successfully.";
+                } else {
+                    echo "Email sending failed.";
+                }
+
+            }
+        }
+
         $postModel  = new PostModel;
         $post = $postModel->getLastPost();
         $this->twig->display('home.html.twig', ['post' => $post]);
