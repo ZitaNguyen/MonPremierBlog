d6c92162ef28d29448f9e58ef152a7cb5326fc20
znguyen <znguyen@getraline.com>
2023-06-28 11:52:55 +0200

    Add function for users to add comment

--- src/Controllers/PostController.php
+++ src/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\AbstractController;
 use App\Models\PostModel;
+use App\Models\UserModel;
 
 class PostController extends AbstractController
 {
@@ -16,8 +17,34 @@ class PostController extends AbstractController
 
     public function getPost($id)
     {
+        if (isset($_POST['submitAddCommentButton']))
+        {
+            if (!empty($_POST['comment']))
+            {
+                $userModel = new UserModel();
+                $aData = [
+                    'comment' => $_POST['comment'],
+                    'person_id' => $_SESSION['user_id'],
+                    'post_id' => $id
+                ];
+
+                $success = $userModel->addComment($aData);
+                if (!$success) {
+                    $_SESSION['message'] = 'Impossible de postuler votre commentaire.';
+                    $_SESSION['error_level'] = 'danger';
+                }
+                header("Location: /post-$id");
+            }
+            else {
+                $_SESSION['message'] = 'Votre commentaire est vide.';
+                $_SESSION['error_level'] = 'info';
+                header("Location: /post-$id");
+            }
+        }
+
         $postModel  = new PostModel();
         $post = $postModel->getPost($id);
-        $this->twig->display('post.html.twig', ['post' => $post]);
+        $comments = $postModel->getComment($id);
+        $this->twig->display('post.html.twig', ['post' => $post, 'comments' => $comments]);
     }
 }
