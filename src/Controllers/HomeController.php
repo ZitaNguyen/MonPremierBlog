fd9bbcd3c2fe8add4f3d67346f4206540d53388a
znguyen <znguyen@getraline.com>
2023-07-10 09:14:07 +0200

    Configuration PHPMailer

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends AbstractController
 {
     public function displayHomePage()
     {
+        var_dump($_POST); die;
         if (isset($_POST['submitContactFormButton']))
         {
             if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message']))
@@ -25,31 +26,22 @@ class HomeController extends AbstractController
                     $mail->isSMTP();                                            //Send using SMTP
                     $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
                     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                    $mail->Username   = 'user@example.com';                     //SMTP username
+                    $mail->Username   = 'zitanguyen84@gmail.com';                     //SMTP username
                     $mail->Password   = 'secret';                               //SMTP password
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                     //Recipients
-                    $mail->setFrom('from@example.com', 'Mailer');
-                    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-                    $mail->addAddress('ellen@example.com');               //Name is optional
-                    $mail->addReplyTo('info@example.com', 'Information');
-                    $mail->addCC('cc@example.com');
-                    $mail->addBCC('bcc@example.com');
-
-                    //Attachments
-                    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-                    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+                    $mail->setFrom($mail->Username, 'Zita Nguyen');
+                    $mail->addAddress($_POST['email'], $_POST['name']);     //Add a recipient
 
                     //Content
                     $mail->isHTML(true);                                  //Set email format to HTML
-                    $mail->Subject = 'Here is the subject';
-                    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-                    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                    $mail->Subject = 'Un email envoyé du blog Zita Nguyen';
+                    $mail->Body    = 'Votre email est bien envoyé.';
 
                     $mail->send();
-                    echo 'Message has been sent';
+                    echo 'Message has been sent'; // message in $_SESSION
                 } catch (Exception $e) {
                     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
                 }
