895b475129003a7f3983367bd08886e82704d566
znguyen <znguyen@getraline.com>
2023-07-26 21:57:39 +0200

    Debug background image of post in admin page

--- src/Models/PostModel.php
+++ src/Models/PostModel.php
@@ -19,8 +19,7 @@ class PostModel
             "SELECT p.id, p.title, p.excerpt, p.image, DATE_FORMAT(p.modify_date, '%d/%m/%Y') AS modifyDate, ps.name
             FROM Post p
             INNER JOIN Person ps ON p.person_id = ps.id
-            ORDER BY create_date DESC
-            LIMIT 5"
+            ORDER BY create_date DESC"
         );
         $sql->execute();
         return $sql->fetchAll(\PDO::FETCH_OBJ);
