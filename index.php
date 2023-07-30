8b41e35e7315dc3b26b3f586a4f95a232a523ffa
znguyen <znguyen@getraline.com>
2023-07-31 00:49:02 +0200

    Refactor index file and add admin comment directly

--- index.php
+++ index.php
@@ -3,68 +3,63 @@ session_start();
 
 include 'vendor/autoload.php';
 
-use App\Controllers\AbstractController;
-use App\Controllers\HomeController;
-use App\Controllers\PostController;
-use App\Controllers\AdminController;
-use App\Controllers\UserController;
-use App\Controllers\ErrorController;
+// use App\Controllers\AbstractController;
+// use App\Controllers\HomeController;
+// use App\Controllers\PostController;
+// use App\Controllers\AdminController;
+// use App\Controllers\UserController;
+// use App\Controllers\ErrorController;
 
-define('ROOT_PATH', __DIR__ . '/src/');
+define('ROOT_PATH', __DIR__.'/src/');
 
+// Parse the requested URL
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = trim($uri, '/');
 
-switch ($uri) {
-case '/':
-				(new HomeController())->displayHomePage();
-					break;
-case '/blog':
-				(new PostController())->getPosts();
-					break;
-case '/login':
-				(new UserController())->login();
-					break;
-case '/register':
-				(new UserController())->register();
-					break;
-case '/logout':
-				(new UserController())->logout();
-					break;
-case '/unset':
-				(new AbstractController())->unsetSession();
-					break;
-case '/admin/posts':
-				(new AdminController())->displayAdminPostsPage();
-					break;
-case '/admin/add-post':
-				(new AdminController())->addPost();
-					break;
-case '/admin/users':
-				(new AdminController())->viewUsers();
-					break;
-case '/admin/comments':
-				(new AdminController())->viewComments();
-					break;
-case preg_match('/[0-9]+/', $uri, $matches):
-				$id = $matches[0];
-				if (strpos($uri, 'modify-post') !== false) {
-					(new AdminController())->modifyPost($id);
-				} elseif (strpos($uri, 'delete-post') !== false) {
-					(new AdminController())->deletePost($id);
-				} elseif (strpos($uri, 'modify-user') !== false) {
-					(new AdminController())->modifyUser($id);
-				} elseif (strpos($uri, 'delete-user') !== false) {
-					(new AdminController())->deleteUser($id);
-				} elseif (strpos($uri, 'validate-comment') !== false) {
-					(new AdminController())->validateComment($id);
-				} elseif (strpos($uri, 'admin') !== false) {
-					(new PostController())->getAdminPost($id);
-				} else {
-					(new PostController())->getPost($id);
-				}
-					break;
-default:
-				(new ErrorController())->get404();
-					break;
-// end switch
+// Define your routes here
+$routes = array(
+    ''                              => 'HomeController@displayHomePage',        // Home page
+    'register'                      => 'UserController@register',               // Create an account
+    'login'                         => 'UserController@login',                  // Connect
+    'logout'                        => 'UserController@logout',                 // Disconnect
+    'unset'                         => 'AbstractController@unsetSession',       // Unset message in session
+    'blog'                          => 'PostController@getPosts',               // Show all blog posts
+    'post-(\d+)'                    => 'PostController@getPost',                // Show a post
+    'admin/posts'                   => 'AdminController@displayAdminPostsPage', // Admin page : Show all blog posts
+    'admin/post-(\d+)'              => 'PostController@getAdminPost',           // Admin page: Show a post
+    'admin/add-post'                => 'AdminController@addPost',               // Admin page : Add an blog post
+    'admin/modify-post-(\d+)'       => 'AdminController@modifyPost',            // Admin page : Edit an blog post
+    'admin/delete-post-(\d+)'       => 'AdminController@deletePost',            // Admin page : Delete an blog post
+    'admin/users'                   => 'AdminController@viewUsers',             // Admin page : Show all users
+    'admin/modify-user-(\d+)'       => 'AdminController@modifyUser',            // Admin page : Edit user role
+    'admin/delete-user-(\d+)'       => 'AdminController@deleteUser',            // Admin page : Delete a user
+    'admin/comments'                => 'AdminController@viewComments',          // Admin page : Show all comments
+    'admin/validate-comment-(\d+)'  => 'AdminController@validateComment',       // Admin page : Validate a comment
+);
+
+// Find the matching route for the requested URL
+$matchedRoute = null;
+foreach ($routes as $route => $controller_action) {
+    // Use preg_match to find a match with regular expression
+    if (preg_match('#^' . $route . '$#', $uri, $matches)) {
+        $matchedRoute = $controller_action;
+        break;
+    }
 }
+
+// If no route matches, handle 404 error
+if ($matchedRoute === null) {
+    // Show error page
+    $matchedRoute = 'ErrorController@get404';
+}
+
+// Remove the first element, as it contains the full match, not just the captured parameters
+array_shift($matches);
+
+// Split the controller and action
+list($controller, $action) = explode('@', $matchedRoute);
+
+// Create the controller instance and call the corresponding action
+$controllerNameSpace = "App\Controllers\\" . $controller;
+$controllerInstance = new $controllerNameSpace();
+call_user_func_array([$controllerInstance, $action], $matches);
--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -7,75 +7,75 @@ use App\Library\Database;
 class UserModel
 {
 
-	protected $Db;
+    protected $Db;
 
 
-	public function __construct()
-	{
-		$this->Db = new Database();
-	}
+    public function __construct()
+    {
+        $this->Db = new Database();
+    }
 
 
-	/**
-	 * Query to add a new user to the database.
-	 */
-	public function register($aData)
-	{
-		$sql = $this->Db->prepare(
-			"INSERT INTO Person (name, email, password, image, role_id)
-			VALUES (:name, :email, :password, :image, :role_id)"
-		);
-		$sql->bindValue(':name', $aData['name'], \PDO::PARAM_STR);
-		$sql->bindValue(':email', $aData['email'], \PDO::PARAM_STR);
-		$sql->bindValue(':password', $aData['password'], \PDO::PARAM_STR);
-		$sql->bindValue(':image', $aData['image'], \PDO::PARAM_STR);
-		$sql->bindValue(':role_id', $aData['role_id'], \PDO::PARAM_INT);
-		return $sql->execute();
-	}
+    /**
+     * Query to add a new user to the database.
+     */
+    public function register($aData)
+    {
+        $sql = $this->Db->prepare(
+            "INSERT INTO Person (name, email, password, image, role_id)
+            VALUES (:name, :email, :password, :image, :role_id)"
+        );
+        $sql->bindValue(':name', $aData['name'], \PDO::PARAM_STR);
+        $sql->bindValue(':email', $aData['email'], \PDO::PARAM_STR);
+        $sql->bindValue(':password', $aData['password'], \PDO::PARAM_STR);
+        $sql->bindValue(':image', $aData['image'], \PDO::PARAM_STR);
+        $sql->bindValue(':role_id', $aData['role_id'], \PDO::PARAM_INT);
+        return $sql->execute();
+    }
 
 
-	/**
-	 * Query to get info of a user by email.
-	 */
-	public function getUser($email)
-	{
-		$sql = $this->Db->prepare(
-			"SELECT p.*, r.role FROM Person p
-			INNER JOIN Role r ON p.role_id = r.id
-			WHERE email = :email");
-		$sql->bindParam(':email', $email);
-		$sql->execute();
-		return $sql->fetch();
-	}
+    /**
+     * Query to get info of a user by email.
+     */
+    public function getUser($email)
+    {
+        $sql = $this->Db->prepare(
+            "SELECT p.*, r.role FROM Person p
+            INNER JOIN Role r ON p.role_id = r.id
+            WHERE email = :email");
+        $sql->bindParam(':email', $email);
+        $sql->execute();
+        return $sql->fetch();
+    }
 
 
-	/**
-	 * Query to add a comment for a post.
-	 */
-	public function addComment($aData)
-	{
-		$sql = $this->Db->prepare(
-			"INSERT INTO Comment (comment, person_id, post_id, validate)
-			VALUES (:comment, :person_id, :post_id, :validate)"
-		);
-		$sql->bindValue(':comment', $aData['comment'], \PDO::PARAM_STR);
-		$sql->bindValue(':person_id', $aData['person_id'], \PDO::PARAM_INT);
-		$sql->bindValue(':post_id', $aData['post_id'], \PDO::PARAM_INT);
-		$sql->bindValue(':validate', $aData['validate'], \PDO::PARAM_INT);
-		return $sql->execute();
-	}
+    /**
+     * Query to add a comment for a post.
+     */
+    public function addComment($aData)
+    {
+        $sql = $this->Db->prepare(
+            "INSERT INTO Comment (comment, person_id, post_id, validate)
+            VALUES (:comment, :person_id, :post_id, :validate)"
+        );
+        $sql->bindValue(':comment', $aData['comment'], \PDO::PARAM_STR);
+        $sql->bindValue(':person_id', $aData['person_id'], \PDO::PARAM_INT);
+        $sql->bindValue(':post_id', $aData['post_id'], \PDO::PARAM_INT);
+        $sql->bindValue(':validate', $aData['validate'], \PDO::PARAM_INT);
+        return $sql->execute();
+    }
 
 
-	/**
-	 * Query to get all users.
-	 */
-	public function getUsers()
-	{
-		$sql = $this->Db->prepare(
-			"SELECT p.*, r.role FROM Person p
-			INNER JOIN Role r ON p.role_id = r.id"
-		);
-		$sql->execute();
-		return $sql->fetchAll();
-	}
+    /**
+     * Query to get all users.
+     */
+    public function getUsers()
+    {
+        $sql = $this->Db->prepare(
+            "SELECT p.*, r.role FROM Person p
+            INNER JOIN Role r ON p.role_id = r.id"
+        );
+        $sql->execute();
+        return $sql->fetchAll();
+    }
 }
